62f1ca9827df39a9d2dcdd4e3ee62d4de7ce4dba
henrik-falk <henrik.falk@netknights.it>
2026-03-17 14:09:31 +0100

    apply review

--- privacyidea/static_new/src/app/components/token/token-find-serial/find-serial-result-dialog/find-serial-result-dialog.component.ts
+++ privacyidea/static_new/src/app/components/token/token-find-serial/find-serial-result-dialog/find-serial-result-dialog.component.ts
@@ -32,7 +32,7 @@ export type GetSerialResultDialogData = {
 export type GetSerialResultDialogReturn = "reset";
 
 @Component({
-  selector: "app-get-serial-result-dialog",
+  selector: "app-find-serial-result-dialog",
   imports: [DialogWrapperComponent],
   templateUrl: "./find-serial-result-dialog.component.html",
   styleUrl: "./find-serial-result-dialog.component.scss",
